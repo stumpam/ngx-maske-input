9e94291b27e04401fa5f442ec0744a59a9616bc9
Martin Štumpa <stumpa.martin@gmail.com>
2020-04-04 20:41:36 +0200

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-masked-input-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'demo';
+}
